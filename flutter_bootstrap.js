6321c27e0ba80904d87d404eb94b2bda295b3606
jb-moon <mooonjbiiin@gmail.com>
2026-09-25 22:25:53 +0900

    Update web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3418560199" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1998126313" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
